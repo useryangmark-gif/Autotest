9e0064239d18546907747273e9025b64b257b1b9
unknown <2194095015@qq.com>
2021-09-02 11:25:50 +0800

    依赖测试

--- Chapter5/src/main/java/com/course/testng/DependTest.java
+++ Chapter5/src/main/java/com/course/testng/DependTest.java
@@ -5,7 +5,9 @@ import org.testng.annotations.Test;
 public class DependTest {
 
     @Test
-    public void test1o(){ System.out.println("test1 befor run"); }
-    @Test(dependsOnMethods = {"test1o"})
+    public void test1(){ System.out.println("test1 befor run");
+        throw new RuntimeException();    //被依赖方法执行失败 依赖的test 会被ignored.
+    }
+    @Test(dependsOnMethods = {"test1"})    //单独运行了test2 test1也会被运行
     public void test2(){ System.out.println("test2 befor run"); }
 }
